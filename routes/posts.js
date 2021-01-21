0694b5735bdfde09ab3bf5fb07729b1260bd8eda
Winston Downes <winston_downes@hotmail.com>
2021-01-21 15:10:58 +1100

    likes working

--- routes/posts.js
+++ routes/posts.js
@@ -4,8 +4,16 @@ const pool = require("../database");
 
 router.get("/:id", function (req, res, next) {
   //   let sql = `SELECT post FROM posts WHERE wall_id=${req.params.id} ORDER BY date ASC`;
-  let sql = `SELECT username, piclocation, post, date FROM posts 
+  let sql = `SELECT posts.id, username, piclocation, post, date, likes FROM posts 
     INNER JOIN members ON members.id = messenger_id AND wall_id=${req.params.id} ORDER BY date ASC`;
+  let query = pool.query(sql, (err, result) => {
+    if (err) throw err;
+    res.json(result);
+  });
+});
+
+router.get("/like/:id", function (req, res, next) {
+  let sql = `UPDATE posts SET likes = likes + 1 WHERE id =${req.params.id}`;
   let query = pool.query(sql, (err, result) => {
     if (err) throw err;
     res.json(result);
